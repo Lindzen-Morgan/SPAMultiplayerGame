1ce08f836cba5146d973e8689b0d8efaed1796c6
Toxicwhisper <morgan.lindzen@gmail.com>
2024-05-23 12:34:32 +0200

    Init commit

---
+++ Data/GameContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class GameContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Game> Games { get; set; }
+
+    public GameContext(DbContextOptions<GameContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Additional configuration
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,9 @@
+﻿public class Game
+{
+    public int Id { get; set; }
+    public string GameState { get; set; }
+    public DateTime LastUpdated { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    // Other game-specific fields
+}
